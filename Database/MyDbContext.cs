bdf66f02f17293b8bf8ee09793cef254d426f726
Clem-Fauchet <cfauchet.pro@gmail.com>
2021-02-05 11:09:44 +0100

    Add database context

---
+++ Database/MyDbContext.cs
@@ -0,0 +1,36 @@
+﻿using ConsoleTP1.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleTP1.Database
+{
+    public class MyDbContext : DbContext
+    {
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<BookShop> BookShops { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Editor> Editors { get; set; }
+
+        public MyDbContext()
+        {
+            if (this.Database.CreateIfNotExists())
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    this.Books.Add(new Entities.Book() { Name = "F" + i, Nb_page = 100 * i });
+                }
+
+                this.SaveChanges();
+      
+            }
+
+
+        }
+
+        
+    }
+}
